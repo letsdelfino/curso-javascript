40058c3a1e1a4ef4c743f684c3ba2835db663434
letsdelfino <41598982+letsdelfino@users.noreply.github.com>
2021-02-18 17:26:59 -0300

    preciso de ajuda....

---
+++ Um exercício ai/script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e){
+
+    // Buscar o valor do input
+    function cadaPessoa(){
+        const nome = e.target.querySelector('#nome');
+        const sobrenome =e.target.querySelector('#sobrenome');
+    }
+
+    const pessoa = [];
+
+    pessoa.forEach(cadaPessoa());
+});
